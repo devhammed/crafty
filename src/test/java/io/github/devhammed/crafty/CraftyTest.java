06fcd8e2fae17853e2d5ff510c36db569d16f128
Hammed Oyedele <itz.harmid@gmail.com>
2023-03-22 17:16:22 +0100

    chore: path to greatness

---
+++ src/test/java/io/github/devhammed/crafty/CraftyTest.java
@@ -0,0 +1,18 @@
+package io.github.devhammed.crafty;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CraftyTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
