11f3ea230d9d9f88197a2e3c4414ad938433a059
Hammed Oyedele <itz.harmid@gmail.com>
2023-03-22 18:36:41 +0100

    feat: add parser class

--- src/main/java/io/github/devhammed/crafty/Crafty.java
+++ src/main/java/io/github/devhammed/crafty/Crafty.java
@@ -59,10 +59,15 @@ public class Crafty {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -73,5 +78,13 @@ public class Crafty {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
